22095417f00ad3151f85dc3a53fca678957cce48
BassemArfaoui <bassem.arfaoui@insat.ucar.tn>
2024-12-13 11:03:47 +0100

    edit errors messages

--- client/src/pages/Prob2.jsx
+++ client/src/pages/Prob2.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import { Modal, Box, Typography, IconButton} from '@mui/material';
-import Select from 'react-select';
-import { notify } from '../components/tools/CustomToaster';
-import '../css/Prob2.css';
-import CloseIcon from '@mui/icons-material/Close';
-import ReplayIcon from '@mui/icons-material/Replay';
+import React, { useState } from "react";
+import axios from "axios";
+import { Modal, Box, Typography, IconButton } from "@mui/material";
+import Select from "react-select";
+import { notify } from "../components/tools/CustomToaster";
+import "../css/Prob2.css";
+import CloseIcon from "@mui/icons-material/Close";
+import ReplayIcon from "@mui/icons-material/Replay";
 
 const selectStyles = {
   control: (provided, state) => ({
@@ -52,91 +52,97 @@ const GridSelector = ({ rows = 25, columns = 71 }) => {
   const togglePointSelection = (x, y) => {
     const transformedX = x - Math.floor(columns / 2);
     const transformedY = Math.floor(rows / 2) - y;
-  
-    const pointExists = selectedPoints.some((point) => point.x === x && point.y === y);
-  
+
+    const pointExists = selectedPoints.some(
+      (point) => point.x === x && point.y === y
+    );
+
     if (pointExists) {
       // Remove the point from both arrays
       setSelectedPoints((prev) =>
         prev.filter((point) => point.x !== x || point.y !== y)
       );
       setSelectedCoordinates((prev) =>
-        prev.filter((coords) => coords[0] !== transformedX || coords[1] !== transformedY)
+        prev.filter(
+          (coords) => coords[0] !== transformedX || coords[1] !== transformedY
+        )
       );
     } else {
       // Check for points in the same column
       const sameColumnPoint = selectedPoints.some((point) => point.x === x);
-  
+
       if (sameColumnPoint) {
-        notify('You cannot select multiple points in the same column!');
+        notify("You cannot select multiple points in the same column!");
         return;
       }
-  
+
       // Add the point to both arrays
       setSelectedPoints((prev) => [...prev, { x, y }]);
       setSelectedCoordinates((prev) => [...prev, [transformedX, transformedY]]);
     }
   };
-  
+
   const sendSelectedPoints = async () => {
     if (selectedCoordinates.length === 0) {
-      notify('Click on the grid to select points !');
+      notify("Click on the grid to select points !");
       return;
     }
-  
+
     if (selectedCoordinates.length < 5) {
-      notify('Please select at least 5 points');
+      notify("Please select at least 5 points");
       return;
     }
-  
-      const maxDistance = 8; 
-      for (let i = 1; i < selectedCoordinates.length; i++) {
-        const [x1, y1] = selectedCoordinates[i - 1];
-        const [x2, y2] = selectedCoordinates[i];
-        const distance = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+
+    const maxDistance = 8;
+
+    // Create a sorted copy of the array based on x-coordinates
+    const sortedCoordinates = [...selectedCoordinates].sort((a, b) => a[0] - b[0]);
     
-        if (distance > maxDistance) {
-          notify('Some points are too far apart!');
-          return;
-        }
+    for (let i = 1; i < sortedCoordinates.length; i++) {
+      const [x1, y1] = sortedCoordinates[i - 1];
+      const [x2, y2] = sortedCoordinates[i];
+      const distance = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+    
+      if (distance > maxDistance) {
+        notify("Some points are too far apart!");
+        return;
       }
-  
-    const formattedData = selectedCoordinates.map((coord) => [coord[0], coord[1]]);
+    }
+    
+
+    const formattedData = selectedCoordinates.map((coord) => [
+      coord[0],
+      coord[1],
+    ]);
     try {
-      const response = await axios.post('http://localhost:9000/polynomial', {
+      const response = await axios.post("http://localhost:9000/polynomial", {
         data_points: formattedData,
         degree: degree,
       });
-  
-      console.log('Polynomial response:', response.data);
+
+      console.log("Polynomial response:", response.data);
       setPolynomialData(response.data);
       setOpenModal(true);
     } catch (error) {
-      console.error('Error sending points:', error);
-      alert('An error occurred while sending points to the server.');
+      console.error("Error sending points:", error);
+      alert("An error occurred while sending points to the server.");
     }
   };
-  
 
   const handleCloseModal = () => {
     setOpenModal(false);
   };
 
-
-  const resetGrid = ()=>
-  {
-    if(selectedPoints.length>0)
-    {
+  const resetGrid = () => {
+    if (selectedPoints.length > 0) {
       setSelectedPoints([]);
       setSelectedCoordinates([]);
       setPolynomialData(null);
       setDegree(3);
+    } else {
+      notify("No points selected !");
     }
-    else
-    {
-      notify('No points selected !');
-    }
-  }
+  };
 
   return (
     <div className="grid-selector">
@@ -233,13 +239,13 @@ const GridSelector = ({ rows = 25, columns = 71 }) => {
             transform: "translate(-50%, -50%)",
             bgcolor: "white",
             p: 3,
-            borderRadius: '10px',
+            borderRadius: "10px",
             boxShadow: 24,
             width: "60%",
             position: "relative",
             paddingTop: "20px",
-            maxHeight : '85vh',
-            overflowY : 'auto'
+            maxHeight: "85vh",
+            overflowY: "auto",
           }}
         >
           <div className="d-flex justify-content-center">
@@ -254,30 +260,31 @@ const GridSelector = ({ rows = 25, columns = 71 }) => {
                 Equation :
               </div>
 
-<div className="d-flex justify-content-center">
-  <span
-    className="my-2 equation px-3 py-2 rounded-3 fw-bold text-center " style={{cursor : 'pointer'}}
-    dangerouslySetInnerHTML={{
-      __html:
-        polynomialData &&
-        polynomialData.coeffs
-          .map((coeff, index) => {
-            const formattedCoeff = coeff.toFixed(7); 
-            if (formattedCoeff === "0.0000000") return null; 
+              <div className="d-flex justify-content-center">
+                <span
+                  className="my-2 equation px-3 py-2 rounded-3 fw-bold text-center "
+                  style={{ cursor: "pointer" }}
+                  dangerouslySetInnerHTML={{
+                    __html:
+                      polynomialData &&
+                      polynomialData.coeffs
+                        .map((coeff, index) => {
+                          const formattedCoeff = coeff.toFixed(7);
+                          if (formattedCoeff === "0.0000000") return null;
 
-            const power = polynomialData.coeffs.length - index - 1;
+                          const power =
+                            polynomialData.coeffs.length - index - 1;
 
-            if (power === 0) return `${formattedCoeff}`; 
-            if (power === 1) return `${formattedCoeff}X`;
-            return `${formattedCoeff}X<sup>${power}</sup>`;
-          })
-          .filter((term) => term) 
-          .join(" + ") 
-          .replace(/\+ -/g, "- "), 
-    }}
-  ></span>
-</div>
-
+                          if (power === 0) return `${formattedCoeff}`;
+                          if (power === 1) return `${formattedCoeff}X`;
+                          return `${formattedCoeff}X<sup>${power}</sup>`;
+                        })
+                        .filter((term) => term)
+                        .join(" + ")
+                        .replace(/\+ -/g, "- "),
+                  }}
+                ></span>
+              </div>
 
               <div>
                 <div className="fs-6 text-secondary text-center mb-0 mt-3 fw-bold ">
