dadd3d98cbe9cfa1b241c9ef906177ccb5bdabe7
BassemArfaoui <bassem.arfaoui@insat.ucar.tn>
2024-12-15 15:38:56 +0100

    add somefiles

---
+++ desktop/electron.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  // Load the React app from the build folder
+  mainWindow.loadURL(path.join('file://', __dirname, 'build', 'index.html'));
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) createWindow();
+});
